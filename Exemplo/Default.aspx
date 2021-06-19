5a9762427cd4c4fc2c77cb4f69fd10f75ac810b1
carlosrodrigooliveira <68301539+carlosrodrigooliveira@users.noreply.github.com>
2021-06-19 16:38:18 -0300

    Add files via upload

---
+++ Exemplo/Default.aspx
@@ -0,0 +1,24 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default"  ValidateRequest="false"%>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <asp:Literal ID="litMensagem" runat="server"></asp:Literal>
+        <hr />
+        Arquivo:  <asp:FileUpload ID="flpArquivo" runat="server" /> 
+        <asp:Button ID="btnImportar" runat="server" Text="Button" OnClick="btnImportar_Click" />
+        <hr />
+        <asp:GridView ID="grvParticpante" runat="server">
+        </asp:GridView>
+        <hr />
+         <asp:GridView ID="grvProduto" runat="server">
+        </asp:GridView>
+    </form>
+</body>
+</html>
---
+++ Exemplo/Default.aspx.cs
@@ -0,0 +1,46 @@
+﻿using Framework.SPED;
+using Framework.SPED.Model.EFD_ICMS;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void btnImportar_Click(object sender, EventArgs e)
+    {
+        try
+        {
+            DateTime inicio = DateTime.Now;
+
+            String caminho = Server.MapPath("~");
+             
+            EFD_ICMS_IPI escrituracao = EFD_ICMS_IPI_BO.GetEscrituracao(flpArquivo.PostedFile.InputStream, caminho, true,true, true);
+
+            //Adiciono o contribuinte como participante
+            escrituracao.Participantes.Add(escrituracao.Entidade);
+
+            grvParticpante.DataSource = escrituracao.Participantes;
+            grvParticpante.DataBind();
+
+            grvProduto.DataSource = escrituracao.Produtos;
+            grvProduto.DataBind();
+
+            TimeSpan ts = DateTime.Now - inicio;
+
+            litMensagem.Text = "Foram encontrados " + escrituracao.Participantes.Count().ToString() + " participantes e " + escrituracao.Produtos.Count().ToString() + " produtos, dados gerados em " + ts.TotalSeconds.ToString() + " segundos.";
+
+        }
+        catch (Exception ex)
+        {
+            litMensagem.Text = ex.Message;
+        }
+    }
+}
